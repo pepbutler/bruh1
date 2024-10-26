7805994d8cac263a61fb680f6dc3be99b8fe4048
James <jameskowal10@gmail.com>
2024-10-26 11:22:55 +0100

    Match poppins font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,3 @@
-//import { Button } from "@/components/ui/button"
-//import { Label } from "@/components/ui/label"
-//import { Input } from "@/components/ui/input"
-//import { Card, CardContent } from "@/components/ui/card"
-//
-//export default function Home() {
-//  return (
-//    <div>
-//      <div className="flex items-center justify-between bg-gray-100 p-1 border-a">
-//        <div className="flex space-x-4">
-//          <Button variant="ghost">Home</Button>
-//          <Button variant="ghost">Projects</Button>
-//          <Button variant="ghost">Settings</Button>
-//        </div>
-//        <div className="flex space-x-4">
-//          <Button variant="outline">Sign In</Button>
-//          <Button>Sign Up</Button>
-//        </div>
-//      </div>
-//      <div className="p-4 flex justify-center">
-//        <Card>
-//          <CardContent>
-//            <div>
-//              <Label htmlFor="learningRate">Learning Rate</Label>
-//              <Input id="learningRate" type="number" placeholder="0.001" />
-//            </div>
-//            <div>
-//              <Label htmlFor="batchSize">Batch Size</Label>
-//              <Input id="batchSize" type="number" placeholder="32" />
-//            </div>
-//            <div>
-//              <Label htmlFor="epochs">Epochs</Label>
-//              <Input id="epochs" type="number" placeholder="10" />
-//            </div>
-//            <Button>Save Settings</Button>
-//          </CardContent>
-//        </Card>
-//      </div>
-//    </div>
-//  )
-//}
-
 import Link from "next/link"
 import {
   Activity,
@@ -85,13 +43,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-
-//export const description = "An application shell with a header and main content area. The header has a navbar, a search input and and a user nav dropdown. The user nav is toggled by a button with an avatar image."
-//
-//export const iframeHeight = "825px"
-//
-//export const containerClassName = "w-full h-full"
-
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col">
